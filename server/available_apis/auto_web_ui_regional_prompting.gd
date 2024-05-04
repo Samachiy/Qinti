7067424424aa0ba1caeb9c6021066be7d6afe5b2
Samachiy <samachiy.r@protonmail.com>
2024-05-04 03:29:37 -0500

    Added shader and colors for regional prompting + Added shader to filter/show only one color + Added array of valid colors for regional prompting

--- server/available_apis/auto_web_ui_regional_prompting.gd
+++ server/available_apis/auto_web_ui_regional_prompting.gd
@@ -1,16 +1,19 @@
 extends DiffusionAPIModule
 
+var colors = [
+	Color().from_hsv(0, 0.5, 0.5),
+	Color().from_hsv(0.5, 0.5, 0.5),
+	Color().from_hsv(0.25, 0.5, 0.5),
+	Color().from_hsv(0.75, 0.5, 0.5),
+	Color().from_hsv(0.125, 0.5, 0.5),
+	Color().from_hsv(0.375, 0.5, 0.5),
+	Color().from_hsv(0.625, 0.5, 0.5),
+	Color().from_hsv(0.875, 0.5, 0.5),
+]
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func bake_regions():
+	# regions_to_bake: Array = [] # [ [rect2_1, data_dict1], [rect2_2, data_dict2], ... ]
+	for i in range(api.regions_to_bake.size(), -1, -1):
+		# Iterate from bottom to top
+		pass
