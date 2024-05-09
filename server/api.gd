b35fb6785876bf3cfc89ad474a1dcb77d8c42192
Samachiy <samachiy.r@protonmail.com>
2024-05-09 09:09:51 -0500

    Moved code to retrieve parse server received image info into the api module

--- server/api.gd
+++ server/api.gd
@@ -290,14 +290,19 @@ func bake_pending_img2img(_cue: Cue):
 # IMAGE INFORMATION
 
 
-func request_image_info(response_object: Object, response_method: String, image_base64: String):
+func request_image_info(_response_object: Object, _response_method: String, _image_base64: String):
 	l.g("The function 'request_image_info' has not been overriden yet on Api: " + 
 	name)
 
 
-func get_image_info_from_result(result):
-	# Returns formated_info
-	# Also, ignore raw_info, we have no use for it
+func get_image_info_from_result(_result):
+	# Returns info formatted like a dictionary, were the keys correspond to the ones found
+	# in Const.gd
+	# An exception to this is:
+	# 	- size (width and height) which comes in the following format:
+	#		- ImageInfoController.SIZE_CONFIG_DISPLAY_NAME: {width} x {height}
+	#	- others (any parameter not found in the Consts.gd) in the following format
+	#		- ImageInfoController.OTHER_DETAILS_KEY: {text/string of the info}
 	l.g("The function 'get_image_info_from_result' has not been overriden yet on Api: " + 
 	name)
 	return {}
